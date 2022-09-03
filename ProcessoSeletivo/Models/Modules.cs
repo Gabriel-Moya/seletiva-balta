7eb7c632fc1ec116d4776e19b63c5071594cb391
Gabriel Moya <gabrielmoya123@gmail.com>
2022-09-03 16:42:13 -0400

    Create modules model

---
+++ ProcessoSeletivo/Models/Modules.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProcessoSeletivo.Models
+{
+    public class Modules
+    {
+        public Modules()
+        {
+            Lessons = new List<Lessons>();
+        }
+
+        [DisplayName("Título")]
+        [Required(ErrorMessage = "Título é obrigatório")]
+        public string Title { get; set; }
+
+        [DisplayName("Descrição")]
+        public string Description { get; set; }
+
+        [DisplayName("Ordem de exibição")]
+        [Required(ErrorMessage = "A ordem da exibição é obrigatória")]
+        public int DisplayOrder { get; set; }
+
+        [DisplayName("Aulas")]
+        public List<Lessons> Lessons { get; set; }
+    }
+}
