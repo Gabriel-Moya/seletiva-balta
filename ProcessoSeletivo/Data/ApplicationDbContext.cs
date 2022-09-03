689017b111db45bdd597a768355e7d0f942e43b6
Gabriel Moya <gabrielmoya123@gmail.com>
2022-09-03 16:24:55 -0400

    Create the project

---
+++ ProcessoSeletivo/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProcessoSeletivo.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
---
+++ ProcessoSeletivo/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProcessoSeletivo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
