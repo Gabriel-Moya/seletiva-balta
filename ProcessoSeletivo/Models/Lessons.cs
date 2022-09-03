dc60a9377097130d7e7b0eef035b5178abe23a34
Gabriel Moya <gabrielmoya123@gmail.com>
2022-09-03 16:27:25 -0400

    Create lesson model

---
+++ ProcessoSeletivo/Models/Lessons.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProcessoSeletivo.Models
+{
+    public class Lessons
+    {
+        [DisplayName("Título")]
+        [Required(ErrorMessage = "Título é obrigatório")]
+        public string Title { get; set; }
+
+        [DisplayName("Descrição")]
+        public string Description { get; set; }
+
+        [DisplayName("URL")]
+        [Required(ErrorMessage = "URL é obrigatória")]
+        public string Url { get; set; }
+    }
+}
