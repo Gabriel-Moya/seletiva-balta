7905515c214d829b78739ab5853684cef8ca7c2c
Gabriel Moya <gabrielmoya123@gmail.com>
2022-09-03 17:40:14 -0400

    Add base entities

---
+++ ProcessoSeletivo/Models/Base/Base.cs
@@ -0,0 +1,12 @@
+﻿namespace ProcessoSeletivo.Models.Base
+{
+    public abstract class Base
+    {
+        public Base()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+    }
+}
---
+++ ProcessoSeletivo/Models/Base/Content.cs
@@ -0,0 +1,12 @@
+﻿namespace ProcessoSeletivo.Models.Base
+{
+    public class Content : Base
+    {
+        public Content(string title)
+        {
+            Title = title;
+        }
+
+        public string Title { get; set; }
+    }
+}
