31619d4d44edabce210e006c2972d20266bbd398
Gabriel Moya <gabrielmoya123@gmail.com>
2022-09-05 18:21:50 -0300

    Authorize controllers

--- ProcessoSeletivo/Controllers/CourseController.cs
+++ ProcessoSeletivo/Controllers/CourseController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProcessoSeletivo.Data;
@@ -5,6 +6,7 @@ using ProcessoSeletivo.Models;
 
 namespace ProcessoSeletivo.Controllers
 {
+    [Authorize]
     public class CourseController : Controller
     {
         private readonly ApplicationDbContext _context;
--- ProcessoSeletivo/Controllers/LessonController.cs
+++ ProcessoSeletivo/Controllers/LessonController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -6,6 +7,7 @@ using ProcessoSeletivo.Models;
 
 namespace ProcessoSeletivo.Controllers
 {
+    [Authorize]
     public class LessonController : Controller
     {
         private readonly ApplicationDbContext _context;
